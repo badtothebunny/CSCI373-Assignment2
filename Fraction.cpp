67880a030cb458529877fdcccaba79c438ef1054
badtothebunny <maria.m.psomas@gmail.com>
2025-09-13 10:13:02 -0400

    Add files via upload

---
+++ Fraction.cpp
@@ -0,0 +1,112 @@
+//Fraction class implementation section
+//Programmer: Maria Psomas :)
+#include "Fraction.h"
+
+//Default constructor
+Fraction::Fraction ()
+{
+	numerator = 0; //Set values to 0.
+	denominator = 0;
+}
+
+//Assignment constructor
+Fraction::Fraction(int num, int den)
+{
+	numerator = num;
+	denominator = den;
+}
+
+//Copy constructor
+Fraction::Fraction(const Fraction &other)
+{
+	numerator = other.numerator;
+	denominator = other.denominator;
+}
+
+//Next are the getFunctions
+int Fraction::getNumerator()const
+{
+	return numerator;
+}
+
+int Fraction::getDenominator()const
+{
+	return denominator;
+} 
+
+//setFunctions
+void Fraction::setNumerator(int num)
+{
+	numerator = num;
+}
+
+void Fraction::setDenominator(int den)
+{
+	denominator = den;
+}
+
+//Operation overloaders
+Fraction Fraction::operator+(const Fraction &add)
+{
+//When it comes to adding fractions, first check if the denominator is equal.
+//If it's not, the numerators are multiplied by the other fraction's denominator.
+	if (denominator != add.denominator)
+		return Fraction((numerator * add.denominator)+(add.numerator * denominator),denominator * add.denominator);
+	else
+		return Fraction (numerator + add.numerator,denominator);
+}
+
+Fraction Fraction::operator-(const Fraction &sub)
+{
+//subtraction will use a similar method.
+	if (denominator != sub.denominator)
+		return Fraction((numerator * sub.denominator)-(sub.numerator * denominator),denominator * sub.denominator);
+	else
+		return Fraction (numerator - sub.numerator,denominator);	
+}
+
+Fraction Fraction::operator*(const Fraction &mul)
+{
+	return Fraction (numerator * mul.numerator, denominator * mul.denominator);
+}
+
+Fraction Fraction::operator/(const Fraction &div)
+{
+	return Fraction (numerator * div.denominator, denominator * div.numerator);
+}
+
+//Print function
+void Fraction::print()const
+{
+	cout<<numerator<<"/"<<denominator<<endl;
+}
+
+//Simplify function
+void Fraction::simplify()
+{
+	int gcd,num,den;
+	num = numerator;
+	den = denominator;
+	if (num > den)
+	{
+		while (num % den != 0)
+		{
+			gcd = num % den;
+			num = den;
+			den = gcd;
+		}
+		numerator = numerator / den;
+		denominator = denominator / den;
+	}
+	else
+	{
+		while (den % num != 0)
+		{
+			gcd = den % num;
+			den = num;
+			num = gcd;
+		}
+		numerator = numerator / num;
+		denominator = denominator / num;
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,56 @@
+//This program focuses on basic operations between fractions
+//Programmer: Maria Psomas
+#include <iostream>
+#include <cstdlib> //For RNG
+#include <ctime>
+#include "Fraction.h"//Including the fraction class I wrote
+using namespace std;
+int main(int argc, char** argv)
+{
+	char operation; //This is for when the user inputs an operation.
+	Fraction first;
+	Fraction answer; //Two Fraction type objects.
+	srand(static_cast<unsigned int>(time(0)));//initializing randomizer
+	cout<<"Welcome :)"<<endl; //Always greet the user :)
+//This while loop is so the user can choose when to end the program
+	while (operation != 'e')
+	{
+//Randomly choosing numbers from 1 to 15
+		first.setNumerator(rand()%15+1);
+		first.setDenominator(rand()%15+1);
+		answer.setNumerator(rand()%15+1);
+		answer.setDenominator(rand()%15+1);
+		cout<<"Numbers are: "<<endl;
+		first.print();
+		answer.print();
+//The following is the menu for the user to select an operation.
+	cout<<"Choose an operation:"<<endl<<"Addition (+)"<<endl
+	<<"Subtraction (-)"<<endl<<"Multiplication (*)"
+	<<endl<<"Division (/)"<<endl<<"Enter 'e' to exit program"<<endl;
+	cin>>operation;
+	switch (operation) //Using switch here to determine what operation to do
+		{
+			case '+':
+				answer = first + answer;
+				cout<<"Sum: "<<endl;
+				break;
+			case '-':
+				answer = first - answer;
+				cout<<"Difference: "<<endl;
+				break;
+			case '*':
+				answer = first * answer;
+				cout<<"Product: "<<endl;
+				break;
+			case '/':
+				answer = first / answer;
+				cout<<"Difference: "<<endl;
+				break;
+			default:
+				cout<<"Thank you for using my program."<<endl;
+				return 0; //Ends program.
+		}
+	answer.simplify();
+	answer.print();
+	}
+}
